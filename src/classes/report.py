581762f542851871629b5d5a7226bb326430916f
Jimi Ademola <108378126+jademola@users.noreply.github.com>
2025-11-29 15:09:42 -0800

    Update such that contribution percentage is only computed if email is not None

--- src/classes/report.py
+++ src/classes/report.py
@@ -168,10 +168,11 @@ class ProjectReport(BaseReport):
             self._find_coding_languages_ratio()
             self._calculate_ari_score()
             self._weighted_skills()
-            if self.project_repo:
-                self._total_contribution_percentage()
+            
             if user_email:
                 self._analyze_git_authorship(user_email)
+                if self.project_repo:
+                    self._total_contribution_percentage()
         else:
             self.project_statistics = statistics
 
