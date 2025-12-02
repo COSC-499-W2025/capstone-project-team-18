1afd862f7113379b10791c157226fca89a1269a0
Sam <sikora.samj@gmail.com>
2025-12-02 21:42:28 +0000

    Added new acitivity type contribution bullet point

--- src/utils/data_processing.py
+++ src/utils/data_processing.py
@@ -33,3 +33,28 @@ def normalize(d: dict[Any, float]) -> None:
 
     for k, v in d.items():
         d[k] = v / total
+
+
+def float_to_percent(f: float) -> str:
+    """
+    Takes a float f in [0,1] and returns
+    a percent that is rounded to one decmial
+    point for text formatting. (e.g. 0.76352 ->
+    76%, 0.01733 -> 2%, 0.0002 -> ~0%)
+
+    Throws error if float is not in [0,1]. Use
+    carefully.
+
+    """
+
+    if not (0 <= f and f <= 1):
+        raise ValueError("Float f not between interval [0,1]")
+
+    f_rounded = round(f * 100)
+
+    percent_str = f"{str(f_rounded)}%"
+
+    if f_rounded == 0:
+        return "~" + percent_str
+
+    return percent_str
