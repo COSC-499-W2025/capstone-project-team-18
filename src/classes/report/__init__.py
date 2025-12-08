e74d45086b95771fe9e0c844a1011673cada451c
Sam <sikora.samj@gmail.com>
2025-12-08 21:15:15 +0000

    Addedd __init__ for report folder

---
+++ src/classes/report/__init__.py
@@ -0,0 +1,25 @@
+"""
+This file acts as a central hub for everything repot.
+This makes it so we can just use
+
+from src.classes.report import FileReport, ProjectReport
+
+instead of
+
+from src.classes.analyzer.file_report import FileReport
+from src.classes.analyzer.project_report import ProjectReport
+
+This makes things a little cleaner.
+"""
+
+from .base_report import BaseReport
+from .file_report import FileReport
+from .project_report import ProjectReport
+from .user_report import UserReport
+
+__all__ = [
+    "BaseReport",
+    "FileReport",
+    "ProjectReport",
+    "UserReport"
+]
