0b9af6a41abe255e70f4073361f48b9ccef4d5b4
Sam <sikora.samj@gmail.com>
2026-01-01 18:37:24 +0000

    Fixed unimported error

--- src/database/utils/database_access.py
+++ src/database/utils/database_access.py
@@ -68,10 +68,10 @@ def get_project_from_project_name(proj_name: str, engine=None) -> ProjectReport:
 
         # Each project name should be unique, throw error if 0 rows or > 1 rows are returned
         except NoResultFound:
-            logging.error(f'Error: No project found with name "{proj_name}"')
+            logger.error(f'Error: No project found with name "{proj_name}"')
             raise
         except MultipleResultsFound:
-            logging.error(
+            logger.error(
                 f'Error: Multiple projects found with name "{proj_name}"')
             raise
 
@@ -180,9 +180,9 @@ def get_user_report(name: str, engine=None) -> UserReport:
 
         # Each project name should be unique, throw error if 0 rows or > 1 rows are returned
         except NoResultFound:
-            logging.error(f'Error: No user report found with name "{name}"')
+            logger.error(f'Error: No user report found with name "{name}"')
             raise
         except MultipleResultsFound:
-            logging.error(
+            logger.error(
                 f'Error: Multiple user reports found with name "{name}"')
             raise
