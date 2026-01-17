077d204c0359839de85e50e7b5b0e9c17e2ef834
Sam <sikora.samj@gmail.com>
2026-01-17 23:49:49 +0000

    Added negative and null result test

--- tests/project_report/project_statistics/test_PR_total_contribution.py
+++ tests/project_report/project_statistics/test_PR_total_contribution.py
@@ -1,24 +1,29 @@
 from pathlib import Path
-from src.classes.analyzer import extract_file_reports
-from src.utils.project_discovery.project_discovery import discover_projects
 import tempfile
-from pathlib import Path
+import pytest
 
 from src.utils.zipped_utils import unzip_file
-from src.utils.project_discovery.project_discovery import discover_projects
 from src.classes.analyzer import extract_file_reports
+from src.utils.project_discovery.project_discovery import discover_projects
 from src.classes.statistic import ProjectStatCollection
 from src.classes.report import ProjectReport
 
 
-def test_verify_accurate_contribution_percentage(resource_dir):
+@pytest.mark.parametrize(
+    "email,percentage",
+    [
+        ("sikora.samj@gmail.com", 100),
+        ("bob@gmail.com", 0),
+        (None, None)
+    ]
+)
+def test_verify_accurate_contribution_percentage(resource_dir, email, percentage):
     """
     Sam has a project in the resource directory that he solo
     made. This test will test to make sure that when we analyze
     this project, it says 100% contribution.
     """
     project_filename = "sample_git_project_one_author.zip"
-    email = "sikora.samj@gmail.com"
 
     zipped_file = Path(resource_dir) / project_filename
     unzipped_dir = tempfile.mkdtemp(prefix="artifact_miner_")
@@ -36,5 +41,5 @@ def test_verify_accurate_contribution_percentage(resource_dir):
         user_email=email
     )
 
-    assert 100 == project_report.get_value(
+    assert percentage == project_report.get_value(
         ProjectStatCollection.TOTAL_CONTRIBUTION_PERCENTAGE.value)
