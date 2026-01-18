e807c7ce25ec9e64b0977a23ba52d342a69d96f2
Jimi Ademola <jimi.ademola1@gmail.com>
2026-01-18 06:40:17 +0000

    Update with additonal constants for future updates

--- src/utils/constants.py
+++ src/utils/constants.py
@@ -1,5 +1,9 @@
 from pathlib import Path
 
-ROOT_PATH: Path = Path(__file__).resolve().parents[2]
+# Allow for non-revealing ease of retrieval for any development paths
+ROOT_PATH: Path = Path(__file__).resolve().parents[3]
+SRC_PATH: Path = Path(__file__).resolve().parents[2]
 
+
+# other paths
 ALEMBIC_PATH = ROOT_PATH
