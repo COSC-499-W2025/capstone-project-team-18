c00bc778cd434d9d3d63179c650edcb11d1dc213
thndlovu <tndlovu@student.ubc.ca>
2026-02-08 23:40:53 -0800

    Deleted unnecessary comment.

--- ui/src/main.tsx
+++ ui/src/main.tsx
@@ -7,7 +7,6 @@ import './index.css'
 
 import './demos/ipc'
 // If you want use Node.js, the`nodeIntegration` needs to be enabled in the Main process.
-// import './demos/node'
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
